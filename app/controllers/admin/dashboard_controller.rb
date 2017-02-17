9bfa1fd0405a723854dd593ef37c7c81ad2576a4
Jeffrey <Jmmercer@ucalgary.ca>
2017-02-17 18:14:38 +0000

    added admin auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+http_basic_authenticate_with name: ENV['ADMIN_BASIC_AUTH_NAME'], password: ENV['ADMIN_BASIC_AUTH_PASSWORD']
   def show
   end
 end
